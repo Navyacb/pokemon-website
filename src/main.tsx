f9303733f82cc840194cbbe28872ab0cd8d144b2
Navyacb <navyacb95@gmail.com>
2024-05-08 11:41:51 +0200

    checking git and vs credential connect

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ const queryClient = new QueryClient({
       retry: false
     }
   }
-});
+}); 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
